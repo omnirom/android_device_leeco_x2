9313c2777366ed4714d17eaabacb91748c3d15e0
Ícaro Hoff <icarohoff@gmail.com>
2017-10-01 10:26:12 +0300

    x2: Add QPerformance to boot jars Improves latency of perflocks.
    Change-Id: I626a2313e60baabcae0bbb549e5519ed67a59cb5

--- device.mk
+++ device.mk
@@ -55,6 +55,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
 
+# QPerformance
+PRODUCT_BOOT_JARS += QPerformance
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
