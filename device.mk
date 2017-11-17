7c2fd58aac62ffed0ee5887f0a88ddf8e118faf7
andr68rus <ashtmb@gmail.com>
2017-11-17 21:18:27 +0300

    x2: add libbt-vendor
    Change-Id: Ia731adce1720102b51d69af838e2f9ad3bdd4ecf

--- device.mk
+++ device.mk
@@ -148,6 +148,10 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_COPY_FILES += \
 #    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbt-vendor
+
 # Camera
 PRODUCT_PACKAGES += \
     libshim_camera \
